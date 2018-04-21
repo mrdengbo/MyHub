689f23a9399bf9728c7f6786e547b1ccbe808d9f
dengbo <1248317408@qq.com>
2018-04-21 16:02:37 +0800

    test1.c

---
+++ hello.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define ERRRETVAL -1
+
+int main()
+{
+	char *p = (char *)malloc(100 * sizeof(char));
+	if (NULL == p)
+	{
+		printf("malloc p is failed\n");
+		return ERRRETVAL;
+	}
+
+	fgets(p,10,stdin);
+
+	printf("hello git\n");
+	printf("%s\n",p);
+	jfdjsajfjsajfjj;
+	jfdjsjafjl;
+	fjfdjsjfj;
+	
+	return 0;
+}
+
+
